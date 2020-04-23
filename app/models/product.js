5c5bd302b7bc516ed04ffb897175a84a4edf29f5
Claudia Lima <limaclaudia@hotmail.com>
2020-04-23 15:42:31 -0300

    install dependencies and create product model

---
+++ app/models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ProductSchema = new Schema (
+    {
+        _id: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true
+        },
+        img: {
+            type: String,
+            required: true
+        }
+    },
+    { versionKey: false }
+)
+
+const Product = mongoose.model('products', ProductSchema)
+module.exports = Product
